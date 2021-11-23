00ff5f128198262eb98d3664fbf5d51424dab77c
Fernando Arias Marqués <fernando.arias@webbeds.com>
2021-11-24 00:17:23 +0100

    Initial version

---
+++ functions.ps1
@@ -0,0 +1,142 @@
+using namespace System.IO
+
+# Create a folder for caching downloads
+$cacheFolder = [Path]::Combine("$PSScriptRoot", ".cache")
+Write-Host "INFO: Cache directory is: $cacheFolder"
+New-Item -ItemType Directory -Force -Path $cacheFolder | Out-Null
+
+Function Get-MyModule {
+    Param(
+        [string]$name
+    )
+
+    if (-not(Get-Module -name $name)) {
+        if (Get-Module -ListAvailable |
+            Where-Object { $_.name -eq $name }) {
+            Import-Module -Name $name
+            $true
+        } 
+        else { $false }
+    } 
+    else { $true }
+} 
+
+
+# 7-zip
+if (!(Get-MyModule -name "7Zip4Powershell")) { 
+    Write-Host -ForegroundColor Cyan "Installing required 7zip module in Powershell"
+    Install-Module -Name "7Zip4Powershell" -Scope CurrentUser -Force 
+}
+Invoke-WebRequest "https://www.7-zip.org/a/7z1900.exe" -Out "$cacheFolder\7z1900.exe"
+Expand-7Zip -ArchiveFileName "$cacheFolder\7z1900.exe" -TargetPath "$cacheFolder\7z\"
+
+Function Get-RemoteFiles {
+    param (
+        [parameter(Mandatory = $true)][string]$jsonFile,
+        [parameter(Mandatory = $true)][string]$targetDir
+    )
+    
+    Get-Content $jsonFile | ConvertFrom-Json | Select-Object -ExpandProperty items | ForEach-Object {
+    
+        $file = $_.file
+        $url = $_.url
+        $repo = $_.repo
+        if($Null -ne $repo) {
+            # This is a GitHub repo. We need to find out the latest tag and then build the URI to that release file
+            $releasesUri = "https://api.github.com/repos/$repo/releases"
+            $releasesResponse = Invoke-WebRequest $releasesUri -UseBasicParsing | ConvertFrom-Json
+            if($Null -ne $releasesResponse) {
+                $tag = $releasesResponse[0].tag_name
+            } else {
+                $tagsUri = "https://api.github.com/repos/$repo/tags"
+                $tagsResponse = Invoke-WebRequest $tagsUri -UseBasicParsing | ConvertFrom-Json
+                $tag = $tagsResponse[0].name
+            }
+            if($Null -ne $url) {
+                $url = $url -replace "{tag}", $tag
+            }
+            else {
+                $url = "https://github.com/$repo/releases/download/$tag/$file"
+            }
+        }
+        $output = "$targetDir\$file"
+        if (![System.IO.File]::Exists($output)) {
+    
+            Write-Host -ForegroundColor Green " Downloading $file..."
+            Invoke-WebRequest $url -Out $output   
+        }
+        else {
+            Write-Host -ForegroundColor Gray " Already downloaded $file... skipped."
+        }
+    }
+}
+
+Function Expand-PackedFile {
+    param (
+        [String]$archiveFile,
+        [String]$targetFolder,
+        [string]$zipFolderToCopy
+    )
+    $tempFolder = New-TemporaryDirectory
+    try {
+        if (Test-Path -LiteralPath $archiveFile) {
+            # Create target directory
+            New-Item -ItemType Directory -Force -Path $targetFolder | Out-Null
+            # Extract to a temp folder
+            Extract -Path $archiveFile -Destination $tempFolder | Out-Null
+            # Move files to the final directory in systems folder
+            if ($zipFolderToCopy -eq "") {
+                Robocopy.exe $tempFolder $targetFolder /E /NFL /NDL /NJH /NJS /nc /ns /np /MOVE | Out-Null
+            }
+            else {
+                Robocopy.exe $tempFolder/$zipFolderToCopy $targetFolder /E /NFL /NDL /NJH /NJS /nc /ns /np /MOVE | Out-Null
+            }
+        }
+        else {
+            Write-Host -ForegroundColor Red "ERROR: $archiveFile not found."
+            exit -1
+        }
+    }
+    finally {
+        if(Test-Path $tempFolder) {
+            Remove-Item $tempFolder -Force -Recurse | Out-Null
+        }
+    }
+    
+}
+
+Function Extract([string]$Path, [string]$Destination) {
+    $sevenZipApplication = "$cacheFolder\7z\7z.exe"
+    $sevenZipArguments = @(
+        'x'                     ## eXtract files with full paths
+        '-y'                    ## assume Yes on all queries
+        "`"-o$($Destination)`"" ## set Output directory
+        "`"$($Path)`""          ## <archive_name>
+    )
+    & $sevenZipApplication $sevenZipArguments | Out-Null
+}
+
+Function Add-Shortcut {
+    param (
+        [String]$ShortcutLocation,
+        [String]$ShortcutTarget,
+        [String]$ShortcutIcon,
+        [String]$WorkingDir
+    )
+    $wshshell = New-Object -ComObject WScript.Shell
+    $link = $wshshell.CreateShortcut($ShortcutLocation)
+    $link.TargetPath = $ShortcutTarget
+    if (-Not [String]::IsNullOrEmpty($WorkingDir)) {
+        $link.WorkingDirectory = $WorkingDir
+    }
+    if (-Not [String]::IsNullOrEmpty($ShortcutIcon)) {
+        $link.IconLocation = $ShortcutIcon
+    }
+    $link.Save() 
+}
+
+Function New-TemporaryDirectory {
+    $parent = [System.IO.Path]::GetTempPath()
+    [string] $name = [System.Guid]::NewGuid()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
---
+++ installer.ps1
@@ -0,0 +1,136 @@
+#Require -Version 5.0
+using namespace System.IO
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [String]
+    $ToolsDir
+)
+
+. (Join-Path $PSScriptRoot functions.ps1)
+
+Write-Host -ForegroundColor Magenta "***************************************"
+Write-Host -ForegroundColor White   "SETUP FOR FERARIAS COMPUTER ENVIRONMENT"
+Write-Host -ForegroundColor Magenta "***************************************"
+
+try {
+    # #############################################################################
+    # SETUP BASIC STUFF
+    # #############################################################################
+    Write-Host -ForegroundColor DarkYellow "SETTING UP REQUIRED PATHS"
+    # Setup some basic directories and stuff
+    Write-Host "INFO: Running from $PSScriptRoot"
+    New-Item -ItemType Directory -Force -Path $ToolsDir | Out-Null
+    Write-Host "INFO: Tools Install directory is $ToolsDir"
+
+    # Set the files that will be downloaded in each section
+    # You can take a look at the "downloads" folder to see which downloads are configured
+    $downloadsFolder = [Path]::Combine("$PSScriptRoot", "downloads")
+    Write-Host "INFO: Downloads directory is: $downloadsFolder."
+    $downloads = @{ 
+        Misc    = [Path]::Combine($downloadsFolder, "misc.json") ;
+    }
+
+    # #############################################################################
+    # MISC ADDITIONAL SOFTWARE
+    # #############################################################################
+    Write-Host -ForegroundColor DarkYellow "INSTALLING ADDITIONAL MISC SOFTWARE"
+    Write-Host -ForegroundColor DarkGreen "Downloading misc additional packages from: $($downloads.Misc)"
+    Get-RemoteFiles $downloads.Misc $cacheFolder
+
+    Expand-PackedFile "$cacheFolder/SysinternalsSuite.zip" ( [Path]::Combine($ToolsDir, "sysinternals") )
+    Copy-Item -Force -Path "$cacheFolder/nuget.exe" -Destination $ToolsDir
+    Copy-Item -Force -Path "$cacheFolder/baretail.exe" -Destination $ToolsDir
+    Copy-Item -Force -Path "$cacheFolder/bombardier-windows-amd64.exe" -Destination "$ToolsDir/bombardier.exe"
+    Copy-Item -Force -Path "$cacheFolder/hey_windows_amd64" -Destination "$ToolsDir/hey.exe"
+    Start-Process "$cacheFolder/wcol500e.exe"
+
+    # Environment Variables
+    [System.Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', [EnvironmentVariableTarget]::Machine)
+
+    # Windows Features
+    # List features: Get-WindowsOptionalFeature -Online
+    Enable-WindowsOptionalFeature -Online -FeatureName 'Containers' -All
+    Enable-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Hyper-V' -All
+    Enable-WindowsOptionalFeature -Online -FeatureName 'VirtualMachinePlatform' -All
+    Enable-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Windows-Subsystem-Linux' -All
+
+    # Terminal
+    winget install --id 'Microsoft.WindowsTerminal' --interactive
+    winget install --id 'Microsoft.Powershell' --interactive --scope machine
+    winget install --id 'JanDeDobbeleer.OhMyPosh' --interactive
+    winget install --id '9P9TQF7MRM4R' --interactive # Windows Subsystem for Linux Preview
+    winget install --id '9NBLGGH4MSV6' --interactive # Ubuntu
+
+    # Office
+    winget install --id '9MSPC6MP8FM4' --interactive # Microsoft Whiteboard
+    winget install --id 'calibre.calibre' --interactive # Calibre
+
+    # Utilities
+    winget install --id '7zip.7zip' --interactive --scope machine
+    winget install --id 'WinSCP.WinSCP' --interactive --scope machine
+    winget install --id 'AgileBits.1Password' --interactive
+    winget install --id 'PuTTY.PuTTY' --interactive --scope machine
+    winget install --id 'ScooterSoftware.BeyondCompare4' --interactive
+
+    # Communication
+    winget install --id '9WZDNCRDK3WP' # Slack
+    winget install --id '9WZDNCRFJ140' # Twitter
+    winget install --id 'XP99J3KP4XZ4VV' # Zoom
+    winget install --id '9N97ZCKPD60Q' # Unigram
+
+    # Browsers
+    winget install --id 'BraveSoftware.BraveBrowser' --interactive
+    winget install --id 'Opera.Opera' --interactive --scope machine
+
+    # Images
+    winget install --id 'Learnpulse.Screenpresso' --interactive --scope machine
+
+    # Media
+    winget install --id 'XPDM1ZW6815MQM' # VLC
+    winget install --id 'plex.plexmediaplayer' --interactive --scope machine
+    winget install --id '9P4CLT2RJ1RS' --interactive # MusicBee
+    winget install --id 'MusicBrainz.Picard' --interactive # MusicBrainz Picard
+
+    # Git
+    winget install --id 'Git.Git' --interactive --scope machine
+    winget install --id 'GitHub.GitLFS' --interactive --scope machine
+    winget install --id 'GitHub.cli' --interactive --scope machine
+    winget install --id 'Axosoft.GitKraken' --interactive
+
+    # Azure/AWS
+    winget install --id 'Microsoft.AzureCLI' --interactive
+    winget install --id 'Microsoft.AzureDataStudio' --interactive --scope machine
+    winget install --id 'Amazon.AWSCLI' --interactive --scope machine
+
+    # Tools
+    # winget install --id 'Docker.DockerDesktop' --interactive --scope machine
+    winget install --id 'Telerik.Fiddler' --interactive
+    winget install --id 'Datalust.Seq' --interactive --scope machine
+    winget install --id '9WZDNCRDMDM3' --interactive # NuGet Package Explorer
+    winget install --id '9MXFBKFVSQ13' --interactive # ILSpy
+
+    # IDE's
+    winget install --id 'Microsoft.VisualStudio.2022.Professional' --interactive
+    winget install --id 'Microsoft.VisualStudioCode' --interactive --scope machine
+    winget install --id 'Postman.Postman' --interactive
+    winget install --id 'HeidiSQL.HeidiSQL' --interactive --scope machine
+    winget install --id '3T.Robo3T' --interactive --scope machine
+
+    # Frameworks
+    winget install --id 'OpenJS.NodeJS' --interactive --scope machine
+    winget install --id 'Microsoft.dotnet' --interactive
+
+    #Synology
+    winget install --id 'Synology.DriveClient' --interactive --scope machine
+    winget install --id 'Synology.NoteStationClient' --interactive --scope machine
+
+    #Specific
+    winget install --id 'eMClient.eMClient' --interactive --scope machine
+
+    Write-Host -ForegroundColor DarkYellow "FINISHED SETUP!"
+}
+catch { 
+    Write-Error $_ 
+}
